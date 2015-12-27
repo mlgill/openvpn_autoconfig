f1e037a995e06f467efff3178b85f45618fb6a77
Andrew Chin <andrewtchin@gmail.com>
2015-12-27 16:37:40 -0600

    Use OpenVPN apt repo.

--- bin/openvpn.sh
+++ bin/openvpn.sh
@@ -35,6 +35,11 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
+RELEASE_CODENAME=$(lsb_release --codename | cut -f2)
+wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
+echo "deb http://swupdate.openvpn.net/apt $RELEASE_CODENAME main" > \
+  /etc/apt/sources.list.d/swupdate.openvpn.net.list
+
 apt-get update -q
 debconf-set-selections <<EOF
 iptables-persistent iptables-persistent/autosave_v4 boolean true
